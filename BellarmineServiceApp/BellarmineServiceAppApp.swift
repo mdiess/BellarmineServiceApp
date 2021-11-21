d660ac2715429fb0c454af015129e61fd692ddbe
Max Diess <max.diess27@gmail.com>
2021-11-21 15:59:01 -0800

    Initial Commit

---
+++ BellarmineServiceApp/BellarmineServiceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BellarmineServiceAppApp.swift
+//  BellarmineServiceApp
+//
+//  Created by Max Diess on 11/21/21.
+//
+
+import SwiftUI
+
+@main
+struct BellarmineServiceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
